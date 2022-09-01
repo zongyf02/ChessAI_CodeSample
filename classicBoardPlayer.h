c6b84226c1bdf5a8dd1eb1d1cc7c00afd7aac2da
zongyf02 <66267484+zongyf02@users.noreply.github.com>
2022-08-31 23:17:10 -0400

    Update classicBoardPlayer.h

--- classicBoardPlayer.h
+++ classicBoardPlayer.h
@@ -21,15 +21,12 @@ class ClassicBoardPlayer: public NegMaxAIPlayer<Coords2D> {
 
         bool operator<(const Zip &other) const; 
     };
-
-    // Heuristics score for moves, range [N_INF * 2, INF]
+    
     int heuristics(Colour curColour, const Move<Coords2D> &move, int depth) const override;
 
-    // Move ordering heuristics
     std::vector<unsigned int> orderMoves(const std::vector<Move<Coords2D>> &moves,
         int depth, bool &isPV) const override;
     
-    // Returns a vector equal to the number of colours/players
     int evaluateBoard(Colour curColour) const override;
 
 public:
